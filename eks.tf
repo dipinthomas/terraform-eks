bbad699d06345440378fc753d47b49794abf6ce0
Dipin Thomas <dipin.thomas@versent.com.sg>
2022-01-11 17:07:16 +0800

    Adding sample kube deploymet

--- eks.tf
+++ eks.tf
@@ -108,3 +108,13 @@ resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
   role       = aws_iam_role.nodegroup.name
 }
 
+
+data "aws_eks_cluster" "cluster" {
+  name = aws_eks_cluster.cluster.id
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = aws_eks_cluster.cluster.id
+}
+
+
